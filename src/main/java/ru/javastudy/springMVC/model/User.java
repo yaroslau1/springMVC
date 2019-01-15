fb2d6da2d5ec1db51b652bfff151823250967814
samoilovich_y <sam.yar@mail.ru>
2019-01-15 12:32:32 +0300

    init

---
+++ src/main/java/ru/javastudy/springMVC/model/User.java
@@ -0,0 +1,27 @@
+package ru.javastudy.springMVC.model;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class User {
+
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
